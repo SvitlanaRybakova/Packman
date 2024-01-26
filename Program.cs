e0c1fa74d733cd91e66c7e5c0e85d78ed03f09e6
Svitlana Rybakova <rsv0011@gmail.com>
2024-01-26 22:01:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Packman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] map = null;
+            string[] file = File.ReadAllLines("map.txt");
+            Console.WriteLine(file[0]);
+        }
+    }
+}
